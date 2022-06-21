8e5353c77cacd79305a08b72e2793453f9315432
Александр Паришкура <pasworkit@gmail.com>
2022-06-21 21:38:09 +0300

    feat: add component header

---
+++ src/scripts/main.js
@@ -0,0 +1,41 @@
+// const burgerBtn = document.querySelector(".header__menu-burger");
+// const closedBtn = document.querySelector(".closed-icon");
+// const headerList = document.querySelector(".header__list");
+// const mobileSize = window.matchMedia('(max-width: 480px)');
+//
+// if (mobileSize.matches) {
+//     console.log(mobileSize.matches)
+//
+//     let clicked = true;
+//     burgerBtn.addEventListener("click", function (e) {
+//         console.log(e)
+//         if (clicked) {
+//             closedBtn.style.display = "block";
+//             burgerBtn.style.display = "none";
+//             headerList.style.display = "block";
+//             clicked = false;
+//             console.log(closedBtn.style.display + 'close')
+//             console.log(burgerBtn.style.display + 'burger')
+//             console.log(headerList.style.display + 'list')
+//         } else {
+//             closedBtn.style.display = "none";
+//             burgerBtn.style.display = "block";
+//             headerList.style.display = "none";
+//             clicked = true;
+//         }
+//     });
+//
+// //     window.addEventListener("click", outsideEvtListener);
+// //
+// //     function outsideEvtListener(e) {
+// //         if (e.target === burgerBtn) {
+// //             return;
+// //         }
+// //         burgerBtn.style.display = "block";
+// //         headerList.style.display = "none";
+// //         closedBtn.style.display = "none";
+// //         clicked = true;
+// //     }
+// }
+//
+//
